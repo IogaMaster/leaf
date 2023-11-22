78c5631e82bc82058786029fa8fed53afc879542
IogaMaster <iogamastercode@gmail.com>
2023-11-22 12:35:08 -0700

    basic fetch

---
+++ src/colors.rs
@@ -0,0 +1,10 @@
+// colors
+pub const CYAN: &str = "\x1b[36m";
+pub const GREEN: &str = "\x1b[32m";
+pub const BLUE: &str = "\x1b[34m";
+pub const WHITE: &str = "\x1B[37m";
+pub const RED: &str = "\x1b[31m";
+pub const YELLOW: &str = "\x1B[32m";
+pub const MAGENTA: &str = "\x1b[35m";
+pub const YELLOW_BRIGHT: &str = "\x1B[93m";
+pub const RESET: &str = "\x1B[0m";
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+mod colors;
+mod utils;
+
+use colors::*;
+use std::{env::var, str::from_boxed_utf8_unchecked};
+use systemstat::{Duration, Platform, System};
+
+const ASCII: &str = "
+   __   _
+ _(  )_( )_
+(_   _    _)
+  (_) (__)";
+
 fn main() {
-    println!("Hello, world!");
+    let sys = System::new();
+
+    // get wm
+    let wm = utils::get_wm().unwrap_or_else(|| String::from("unknown"));
+
+    // get terminal
+    let term = var("TERM").unwrap_or_else(|_| String::from("unknown"));
+
+    // get shell
+    let shell = utils::get_shell().unwrap_or_else(|| String::from("unknown"));
+
+    // get uptime
+    let uptime = sys.uptime().unwrap_or_else(|_| Duration::default());
+
+    // format fetch text
+    let fetch_text = vec![
+        format!(
+            "{WHITE}{}{RED}@{RESET}{}{BLUE}",
+            whoami::username(),
+            whoami::hostname()
+        ),
+        format!("{CYAN}os {WHITE}  ~ {CYAN}{}{BLUE}", whoami::distro()),
+        format!("{YELLOW}up{WHITE} ~ {YELLOW}{uptime:?}{BLUE}"),
+        format!("{GREEN}wm {WHITE}  ~ {GREEN}{wm}{BLUE}"),
+        format!("{MAGENTA}term{WHITE} ~ {MAGENTA}{term}{BLUE}"),
+        format!("{YELLOW_BRIGHT}sh {WHITE}  ~ {YELLOW_BRIGHT}{shell}{BLUE}"),
+        format!("{RED}● {YELLOW}● {CYAN}● {BLUE}● {WHITE}●"),
+    ]
+    .join("\n");
+
+    println!("{fetch_text}");
 }
